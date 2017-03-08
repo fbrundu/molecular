ad3220e4e414953f55411b52e557a353525107f6
Francesco G. Brundu <francesco.brundu@gmail.com>
2017-03-08 12:48:32 +0100

    Plot library, to test

--- molecular/plotting/__init__.py
+++ molecular/plotting/__init__.py
@@ -3,5 +3,4 @@
 __author__ = """Francesco G. Brundu"""
 __email__ = 'francesco.brundu@gmail.com'
 
-from .plotting import Plot
-from .preprocessing import FeatureConstruction, Mapping
+from .plot import Plot
