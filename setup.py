cd3de6550d2a0d723aa9c00ce06ae482c8e2f920
Francesco G. Brundu <francesco.brundu@gmail.com>
2017-03-08 14:08:19 +0100

    Packaging

--- setup.py
+++ setup.py
@@ -4,50 +4,55 @@
 from setuptools import setup
 
 with open('README.rst') as readme_file:
-    readme = readme_file.read()
+  readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+  history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+  bioservices==1.4.16,
+  pandas==0.19.2,
+  scipy==0.18.1,
+  plotly==2.0.5,
+  seaborn==0.7.1,
+  IPython==5.3.0,
+  statsmodels==0.8.0,
+  numpy==1.12.0,
+  ipywidgets==6.0.0,
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+  # TODO: put package test requirements here
 ]
 
 setup(
-    name='molecular',
-    version='0.1.0',
-    description="Computational Biology utility module",
-    long_description=readme + '\n\n' + history,
-    author="Francesco G. Brundu",
-    author_email='francesco.brundu@gmail.com',
-    url='https://github.com/fbrundu/molecular',
-    packages=[
-        'molecular',
-    ],
-    package_dir={'molecular':
-                 'molecular'},
-    include_package_data=True,
-    install_requires=requirements,
-    license="MIT license",
-    zip_safe=False,
-    keywords='molecular',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
+  name='molecular',
+  version='0.1.0',
+  description="Computational Biology utility module",
+  long_description=readme + '\n\n' + history,
+  author="Francesco G. Brundu",
+  author_email='francesco.brundu@gmail.com',
+  url='https://github.com/fbrundu/molecular',
+  packages=[
+    'molecular',
+    'molecular.histology',
+    'molecular.plotting',
+    'molecular.preprocessing',
+  ],
+#  package_dir={'molecular':
+#               'molecular'},
+  include_package_data=True,
+  install_requires=requirements,
+  license="MIT license",
+  zip_safe=False,
+  keywords='molecular',
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 3.6',
+  ],
+  test_suite='tests',
+  tests_require=test_requirements
 )
